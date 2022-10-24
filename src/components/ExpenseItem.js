1ca39878115a29376b90058318d759afa8807a52
Vincent van Rootselaar <86999767+vincentvanrootselaar@users.noreply.github.com>
2022-10-24 21:13:58 +0200

    FirstCommit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item" >
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+      </div>
+      <div className="expense-item__price">Amount</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
